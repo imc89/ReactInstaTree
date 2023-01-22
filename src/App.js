ee6cff55095cf90c95285a050a8b2ae8f01b042d
Iñigo Montánchez <mci.m89@gmail.com>
2023-01-22 16:16:27 +0100

    feat: add img folder with desktop and mobile backgrounds, add useWindowWidth function to change the backgrounds depends on the sizes. Changes in App styles.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import desktopImage from './img/desktop.jpg';
+import mobileImage from './img/mobile.jpg';
+
 import './App.css';
 
 function App() {
+  const imageUrl = useWindowWidth() >= 650 ? desktopImage : mobileImage;
+
   return (
-    <div className="App">
+    <div className="App" style={{backgroundImage: `url(${imageUrl})` }}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
+  
 }
+
+const useWindowWidth = () => {
+  const [windowWidth, setWindowWidth ] = useState(window.innerWidth);
+
+  useEffect(() => {
+      const handleWindowResize = () => {
+          setWindowWidth(window.innerWidth);
+      };
+
+      window.addEventListener('resize', handleWindowResize);
+      return () => window.removeEventListener('resize', handleWindowResize);
+  },[]);
+
+  return windowWidth;
+};
 
 export default App;
